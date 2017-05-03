bd5b3f1f8fa92b0647e871804fa243b7075ac54d
tmill <tim.miller@gmail.com>
2017-05-03 09:59:49 -0400

    Added a second adaptation prediction variable.

--- scripts/compute_adaptation_variables.py
+++ scripts/compute_adaptation_variables.py
@@ -4,6 +4,7 @@ import sys
 from sklearn.datasets import load_svmlight_file
 from sklearn.metrics import accuracy_score
 import scipy.sparse
+import scipy.stats
 from uda_common import zero_pivot_columns, zero_nonpivot_columns, read_pivots, evaluate_and_print_scores, align_test_X_train, get_f1, find_best_c
 
 def main(args):
@@ -23,6 +24,16 @@ def main(args):
     domain_y[:num_source_instances] = 1
     c, acc = find_best_c(combined_X, domain_y, scorer=accuracy_score)
     print("Best performance for telling domains apart is %f" % (acc))
+
+    ## Second adaptation variable: cosine similarity between vector averages
+    source_ave = np.average(source_X.toarray(), 0)
+    target_ave = np.average(target_X.toarray(), 0)
+    dot_sim = source_ave.dot(target_ave)
+    # This function computes kl-divergence if given 2 arguments and does the normalizatino for you:
+    kl = scipy.stats.entropy(source_ave, target_ave)
+    print("Raw dot product is %f, kl divergence is %f" % (dot_sim, kl))
+
+
 if __name__ == '__main__':
     args = sys.argv[1:]
     main(args)
