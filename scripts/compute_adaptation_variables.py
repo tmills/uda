b05e2f808de4cb8b6261ee37e1611e0f1c4ec3f1
Tim Miller <timothy.miller@childrens.harvard.edu>
2017-05-22 17:24:45 -0400

    Added prevalence computing variable and commented out slow scl prediction code.

--- scripts/compute_adaptation_variables.py
+++ scripts/compute_adaptation_variables.py
@@ -5,7 +5,7 @@ from sklearn.datasets import load_svmlight_file
 from sklearn.metrics import accuracy_score
 import scipy.sparse
 import scipy.stats
-from uda_common import zero_pivot_columns, zero_nonpivot_columns, read_pivots, evaluate_and_print_scores, align_test_X_train, get_f1, find_best_c
+from uda_common import zero_pivot_columns, zero_nonpivot_columns, read_pivots, evaluate_and_print_scores, align_test_X_train, get_f1, find_best_c, get_preds
 from sklearn import svm
 
 def main(args):
@@ -13,6 +13,7 @@ def main(args):
         sys.stderr.write("Two required arguments: <source data> <target data> <pivot index file>\n")
         sys.exit(-1)
 
+    goal_ind = 2
     source_X, source_y = load_svmlight_file(args[0])
     num_source_instances, num_features = source_X.shape
     target_X, target_y = load_svmlight_file(args[1])
@@ -41,24 +42,31 @@ def main(args):
     kl = scipy.stats.entropy(source_ave+0.01, target_ave+0.01)
     print("Raw dot product is %f, cos sim is %f, kl divergence is %f" % (dot_sim, cos_sim, kl))
 
+    ## Get prevalences for source and target
+    c, acc = find_best_c(source_X, source_y, scorer_args={'goal_ind':goal_ind})
+    target_preds = get_preds(source_X, source_y, target_X, C=c)
+    gold_prev = len(np.where(source_y==goal_ind)[0]) / float(len(source_y))
+    target_prev = len(np.where(target_preds==goal_ind)[0]) / float(len(target_preds))
+    print("Gold goal prevalence is %f compared to predicted target prevalence of %f" % (gold_prev, target_prev))
+
     # Last variable is how well can we predict pivot features with non-pivot features:
-    pivots = read_pivots(args[2])
-    nopivot_X_train = zero_pivot_columns(combined_X, pivots).toarray()
-    accuracies = np.zeros(len(pivots))
-
-    for ind,pivot in enumerate(pivots):
-        ## build a test set with pivot values:
-        pivot_y = np.zeros(combined_X.shape[0]) + combined_X[:,pivot].toarray()[:,0]
-        if not len(pivot_y) == (len(np.where(pivot_y == 1.0)[0]) + len(np.where(pivot_y == 0.0)[0])):
-            print("Feature %d is not restricted to 0/1 so we are skipping" % (pivot))
-            continue
-        c, acc = find_best_c(nopivot_X_train, pivot_y, scorer=accuracy_score, C_list = [1.0])
-        accuracies[ind] = acc
-        print("Debugging: Accuracy for pivot %d is %f" % (ind, acc))
-
-    ave_acc = accuracies.mean()
-    med_acc = np.median(accuracies)
-    print("Average accuracy at pivot prediction is %f, median accuracy is %f" %(ave_acc, med_acc))
+    # pivots = read_pivots(args[2])
+    # nopivot_X_train = zero_pivot_columns(combined_X, pivots).toarray()
+    # accuracies = np.zeros(len(pivots))
+    #
+    # for ind,pivot in enumerate(pivots):
+    #     ## build a test set with pivot values:
+    #     pivot_y = np.zeros(combined_X.shape[0]) + combined_X[:,pivot].toarray()[:,0]
+    #     if not len(pivot_y) == (len(np.where(pivot_y == 1.0)[0]) + len(np.where(pivot_y == 0.0)[0])):
+    #         print("Feature %d is not restricted to 0/1 so we are skipping" % (pivot))
+    #         continue
+    #     c, acc = find_best_c(nopivot_X_train, pivot_y, scorer=accuracy_score, C_list = [1.0])
+    #     accuracies[ind] = acc
+    #     print("Debugging: Accuracy for pivot %d is %f" % (ind, acc))
+    #
+    # ave_acc = accuracies.mean()
+    # med_acc = np.median(accuracies)
+    # print("Average accuracy at pivot prediction is %f, median accuracy is %f" %(ave_acc, med_acc))
 
 
 if __name__ == '__main__':
