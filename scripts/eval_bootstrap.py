b2b30d767cd6fc72d09d34ca0d2eab3653b1982e
Tim Miller <timothy.miller@childrens.harvard.edu>
2018-02-26 15:39:28 -0500

    Add new method for using silver instances for tuning c.

--- scripts/eval_bootstrap.py
+++ scripts/eval_bootstrap.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import numpy as np
+import numpy.random
 import os
 from os.path import join,exists,dirname
 from sklearn import svm
@@ -50,31 +51,74 @@ def main(args):
         (l2_c, l2_f1) = find_best_c(X_train, y_train, pos_label=goal_ind)
         print("Optimizing l2 with cross-validation gives C=%f and f1=%f" % (l2_c, l2_f1))
 
-        print("Tuning regularization parameter on gold+silver:")
-        svc = svm.LinearSVC(penalty='l2', C=l2_c)
-        svc.fit(X_train, y_train)
-        preds = svc.predict(X_test)  # predict should return 0/1 (maybe experiment with weights?)
-        decisions = svc.decision_function(X_test)
 
-        for threshold in (0.0, 1.0):
-            sys.stderr.write("Testing silver optimization with confidence threshold %f" % threshold)
-            confident_indices = np.where(np.abs(decisions) > threshold)[0]
-            num_added = len(confident_indices)
-
-            train_plus_test_X = np.zeros((num_train_instances+num_added, num_feats))
-            train_plus_test_X[:num_train_instances, :] += X_train
-            train_plus_test_X[num_train_instances:, :] += X_test[confident_indices, :]
-            train_plus_test_y = np.zeros(num_train_instances+num_added)
-            train_plus_test_y[:num_train_instances] += y_train
-            train_plus_test_y[num_train_instances:] += preds[confident_indices]
-            (gs_l2_c, gs_l2_f1) = find_best_c(train_plus_test_X, train_plus_test_y, pos_label=goal_ind)
-            print(" Optimized l2 for gold+silver: %f" % (gs_l2_c))
-            evaluate_and_print_scores(X_train, y_train, X_test, y_test, goal_ind, gs_l2_c)
-            del train_plus_test_X, train_plus_test_y
-
-        run_balanced_bootstrapping(X_train, y_train, X_test, y_test, goal_ind)
+        run_oracle_silver_tuning(X_train, y_train, X_test, y_test, goal_ind, l2_c)
+        #run_silver_tuning(X_train, y_train, X_test, y_test, goal_ind)
+        #run_balanced_bootstrapping(X_train, y_train, X_test, y_test, goal_ind)
         #run_enriched_bootstrapping(X_train, y_train, X_test, y_test, goal_ind)
 
+def run_oracle_silver_tuning(X_train, y_train, X_test, y_test, goal_ind, l2_c):
+    print("Tuning regularization using oracle labels from confident/unconfident instances")
+    num_train_instances = X_train.shape[0]
+    num_feats = X_train.shape[1]
+    num_test_instances = X_test.shape[0]
+
+    num_added = 100
+    svc = svm.LinearSVC(penalty='l2', C=l2_c)
+    svc.fit(X_train, y_train)
+    # preds = svc.predict(X_test)  # predict should return 0/1 (maybe experiment with weights?)
+    decisions = svc.decision_function(X_test)
+    # confidence is signed, so we take the absolute value to get most/least confident
+    # decisions in either direction
+    confidence_indices = np.argsort(np.abs(decisions))
+
+    for config in ('most_confident', 'least_confident', 'random'):
+        if config == 'most_confident':
+            target_indices = confidence_indices[-num_added:]
+        elif config == 'least_confident':
+            target_indices = confidence_indices[:num_added]
+        else:
+            target_indices = np.random.choice(num_test_instances, num_added, replace=False)
+
+        train_plus_test_X = np.zeros((num_train_instances+num_added, num_feats))
+        train_plus_test_X[:num_train_instances, :] += X_train
+        train_plus_test_X[num_train_instances:, :] += X_test[target_indices, :]
+        train_plus_test_y = np.zeros(num_train_instances+num_added)
+        train_plus_test_y[:num_train_instances] += y_train
+        train_plus_test_y[num_train_instances:] += y_test[target_indices]
+        (gs_l2_c, gs_l2_f1) = find_best_c(train_plus_test_X, train_plus_test_y, pos_label=goal_ind)
+        print(" Optimized l2 for gold+silver with %d %s instances added: %f" % (num_added, config, gs_l2_c))
+        evaluate_and_print_scores(X_train, y_train, X_test, y_test, goal_ind, gs_l2_c)
+        del train_plus_test_X, train_plus_test_y
+
+
+
+
+def run_silver_tuning(X_train, y_train, X_test, y_test, goal_ind):
+    print("Tuning regularization parameter on gold+silver:")
+    num_train_instances = X_train.shape[0]
+    num_feats = X_train.shape[1]
+    num_test_instances = X_test.shape[0]
+
+    svc = svm.LinearSVC(penalty='l2', C=l2_c)
+    svc.fit(X_train, y_train)
+    preds = svc.predict(X_test)  # predict should return 0/1 (maybe experiment with weights?)
+    decisions = svc.decision_function(X_test)
+    for threshold in (0.0, 1.0):
+        sys.stderr.write("Testing silver optimization with confidence threshold %f" % threshold)
+        confident_indices = np.where(np.abs(decisions) > threshold)[0]
+        num_added = len(confident_indices)
+
+        train_plus_test_X = np.zeros((num_train_instances+num_added, num_feats))
+        train_plus_test_X[:num_train_instances, :] += X_train
+        train_plus_test_X[num_train_instances:, :] += X_test[confident_indices, :]
+        train_plus_test_y = np.zeros(num_train_instances+num_added)
+        train_plus_test_y[:num_train_instances] += y_train
+        train_plus_test_y[num_train_instances:] += preds[confident_indices]
+        (gs_l2_c, gs_l2_f1) = find_best_c(train_plus_test_X, train_plus_test_y, pos_label=goal_ind)
+        print(" Optimized l2 for gold+silver: %f" % (gs_l2_c))
+        evaluate_and_print_scores(X_train, y_train, X_test, y_test, goal_ind, gs_l2_c)
+        del train_plus_test_X, train_plus_test_y
 
 
 def run_balanced_bootstrapping(X_train, y_train, X_test, y_test, goal_ind):
