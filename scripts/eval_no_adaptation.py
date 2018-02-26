08f5694156b512c2582816aac1f568d40ffa56bc
Tim Miller <timothy.miller@childrens.harvard.edu>
2018-02-26 10:58:04 -0500

    Add prevalence info to no adaptation, and search a wider gamut of c values.

--- scripts/eval_no_adaptation.py
+++ scripts/eval_no_adaptation.py
@@ -41,19 +41,29 @@ def main(args):
         y_test = all_y[test_instance_inds]
         num_test_instances = X_test.shape[0]
 
+        source_prevalence = len(np.where(y_train == goal_ind)[0]) / float(len(y_train))
+        sys.stderr.write("Source prevalence is: %f\n" % source_prevalence)
+        target_prevalence = len(np.where(y_test == goal_ind)[0]) / float(len(y_test))
+        sys.stderr.write("Target prevalence is: %f\n" % target_prevalence)
+        
+
         (l2_c, l2_f1) = find_best_c(X_train, y_train, scorer=f1_score, pos_label=goal_ind)
+        # p,r,f,_ = cross_val_score(svm.LinearSVC(C=C, penalty=penalty, dual=dual), X_train, y_train, scoring=scorer, n_jobs=1))
         print("Tuning on source selected c value %f" % (l2_c))
 
         clf = svm.LinearSVC(C=l2_c)
         clf.fit(X_train, y_train)
         y_predicted = clf.predict(X_test)
+        predicted_prevalence = len(np.where(y_predicted == goal_ind)[0]) / float(len(y_test))
+        sys.stderr.write("Predicted (target) prevalence is %f\n" % (predicted_prevalence))
+
         # f1 = f1_score(y_test, y_predicted, pos_label=goal_ind)
         p,r,f1,_ = precision_recall_fscore_support(y_test, y_predicted, average='binary', pos_label=goal_ind)
         print("Precision,recall,F-score on test data is %f\t%f\t%f" % (p,r,f1))
 
-        best_c = best_f = best_r = best_p = 0
-        for c_exp in range(-3, 4):
-            c = 10. ** c_exp
+        best_c = best_f = best_r = best_p = predicted_prevalence = 0
+        for c_exp in range(-10, 0):
+            c = 2. ** c_exp
             #print("Testing with c=%f" % (c))
             clf = svm.LinearSVC(C=c)
             clf.fit(X_train, y_train)
@@ -65,8 +75,9 @@ def main(args):
                 best_r = r
                 best_p = p
                 best_c = c
+                predicted_prevalence = len(np.where(y_predicted == goal_ind)[0]) / float(len(y_test))
 
-        print("P/R/F if we tune c=%f to optimize test set F score is %f\t%f\t%f"%  (best_c, best_p, best_r, best_f))
+        print("P/R/F if we tune c=%f to optimize test set F score is %f\t%f\t%f with prevalence %f"%  (best_c, best_p, best_r, best_f, predicted_prevalence))
 
 
 
